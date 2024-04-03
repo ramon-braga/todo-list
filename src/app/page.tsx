de70a111f03ce25bd828d19a494112153b842eaa
Ramon Braga <140903176+ramon-braga@users.noreply.github.com>
2024-04-03 19:14:46 -0300

    tasks now are loaded from todoList array and UI design was changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,25 +13,25 @@ function Page() {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen gap-3">
       
-      <div className="bg-blue-950 rounded-md p-3">
-        <h1 className="text-3xl mb-3 text-center">Todo List</h1>
+      <div className="bg-black border border-white rounded-md p-9">
+        <h1 className="text-4xl mb-6 text-center font-serif">Todo List</h1>
         <input 
           type="text"
           className="bg-transparent p-2 outline-none border-b border-white text-white rounded-sm"
           placeholder="What to do?"
         />
-        <button className="ml-3 bg-teal-500 py-2 px-5 rounded-sm active:bg-teal-400">Add</button>
-        <ul className="mt-3 flex flex-col gap-3">
-          
-          <li className="rounded-sm flex items-center justify-between">
-            <p className="inline pl-2">Task 1</p><button className="bg-pink-500 py-1 px-3 rounded-sm active:bg-pink-400">Delete</button>
-          </li>
-
-          <li className="rounded-sm flex items-center justify-between">
-            <p className="inline pl-2">Task 2</p><button className="bg-pink-500 py-1 px-3 rounded-sm active:bg-pink-400">Delete</button>
-          </li>
+        <button className="ml-3 bg-red-500 py-2 px-5 rounded-sm active:bg-red-400">Add</button>
         
+        <ul className="mt-9 flex flex-col gap-4">
+
+        {list.map(task =>
+          <li className="rounded-sm flex items-center justify-between">
+             <p className="inline pl-2">{task.label}</p><button className="bg-transparent border border-red-500 py-1 px-3 rounded-sm active:bg-red-400">Delete</button>
+          </li>
+        )}
+
         </ul>
+
       </div>
 
     </div>
